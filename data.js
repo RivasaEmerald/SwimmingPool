1a2e190e776e3baa578cc38fda46a8111707aa74
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-19 11:39:21 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.21397494459110256,
+        "pitch": 0.0066039206012717955,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11226013289495462,
+          "pitch": 0.03666683012188443,
+          "rotation": 1.5707963267948966,
+          "target": "1-2"
+        },
+        {
+          "yaw": 1.528902987716128,
+          "pitch": 0.007957336284231786,
+          "rotation": 4.71238898038469,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.411020484470855,
+        "pitch": 0.10755892525721578,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4046347313086294,
+          "pitch": 0.03200529071895275,
+          "rotation": 1.5707963267948966,
+          "target": "3-4"
+        },
+        {
+          "yaw": -3.0044611555317786,
+          "pitch": -0.0036660322619894714,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -3.037453096069287,
+        "pitch": 0.015343148519679772,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.141779013890204,
+          "pitch": -0.021815112310248352,
+          "rotation": 4.71238898038469,
+          "target": "3-4"
+        },
+        {
+          "yaw": 2.0075188970086213,
+          "pitch": 0.007821181537822852,
+          "rotation": 4.71238898038469,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.1202622305846237,
+        "pitch": 0.16623438313247618,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4312447784174154,
+          "pitch": 0.02126011010608231,
+          "rotation": 10.995574287564278,
+          "target": "1-2"
+        },
+        {
+          "yaw": -3.1365624658143183,
+          "pitch": 0.00398728730957032,
+          "rotation": 1.5707963267948966,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Rivasa Emerald Swimming Pool  ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
